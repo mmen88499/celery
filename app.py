545f481a8a8fbc98c3405e1e7f912cf6537fd6c4
mmen88499 <145101430+mmen88499@users.noreply.github.com>
2024-06-21 15:57:47 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,151 +1,97 @@
-#****************************************************
-#                                                   *
-#               HTTP PROXY                          *
-#               Version: 1.0                        *
-#               Author: Luu Gia Thuy                *
-#                                                   *
-#****************************************************
+import socket
+import threading
 
-import os,sys,thread,socket
+# Constant variables
+BACKLOG = 50
+MAX_DATA_RECV = 999999
+DEBUG = True
+BLOCKED = []  # example: ["www.blocked.com"]
 
-#********* CONSTANT VARIABLES *********
-BACKLOG = 50            # how many pending connections queue will hold
-MAX_DATA_RECV = 999999  # max number of bytes we receive at once
-DEBUG = True            # set to True to see the debug msgs
-BLOCKED = []            # just an example. Remove with [""] for no blocking at all.
-
-#**************************************
-#********* MAIN PROGRAM ***************
-#**************************************
-def main():
-
-    if (len(sys.argv)<2):
-        print "No port given, using :8080 (http-alt)" 
-        port = 8080
-    else:
-        port = int(sys.argv[1]) # port from argument
-if (len(sys.argv)<2):
-print "No port given, using :8080 (http-alt)" 
-        port = 8080
-    else:
-        port = int(sys.argv[1]) # port from argument
-
-    # host and port info.
-    host = ''               # blank for localhost
-
-    print "Proxy Server Running on ",host,":",port
-
-    try:
-        # create a socket
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-        # associate the socket to host and port
-        s.bind((host, port))
-
-        # listenning
-        s.listen(BACKLOG)
-
-    except socket.error, (value, message):
-        if s:
-            s.close()
-        print "Could not open socket:", message
-        sys.exit(1)
-
-    # get the connection from client
-    while 1:
-        conn, client_addr = s.accept()
-
-        # create a thread to handle request
-        thread.start_new_thread(proxy_thread, (conn, client_addr))
-
-    s.close()
-#************** END MAIN PROGRAM ***************
-
-def printout(type,request,address):
+def printout(type, request, address):
     if "Block" in type or "Blacklist" in type:
         colornum = 91
     elif "Request" in type:
         colornum = 92
     elif "Reset" in type:
         colornum = 93
+    print(f"\033[{colornum}m{address[0]}\t{type}\t{request}\033[0m")
 
-    print "\033[",colornum,"m",address[0],"\t",type,"\t",request,"\033[0m"
-
-#*******************************************
-#********* PROXY_THREAD FUNC ***************
-# A thread to handle request from browser
-#*******************************************
 def proxy_thread(conn, client_addr):
-
-    # get the request from browser
     request = conn.recv(MAX_DATA_RECV)
+    first_line = request.split(b'\n')[0]
+    url = first_line.split(b' ')[1]
 
-    # parse the first line
-    first_line = request.split('\n')[0]
-
-    # get url
-    url = first_line.split(' ')[1]
-
-    for i in range(0,len(BLOCKED)):
-        if BLOCKED[i] in url:
-            printout("Blacklisted",first_line,client_addr)
+    for blocked in BLOCKED:
+        if blocked in url:
+            printout("Blacklisted", first_line, client_addr)
             conn.close()
-            sys.exit(1)
+            return
 
+    printout("Request", first_line, client_addr)
 
-    printout("Request",first_line,client_addr)
-    # print "URL:",url
-    # print
-
-    # find the webserver and port
-    http_pos = url.find("://")          # find pos of ://
-    if (http_pos==-1):
+    http_pos = url.find(b"://")
+    if http_pos == -1:
         temp = url
     else:
-        temp = url[(http_pos+3):]       # get the rest of url
+        temp = url[http_pos + 3:]
 
-    port_pos = temp.find(":")           # find the port pos (if any)
-
-    # find end of web server
-    webserver_pos = temp.find("/")
+    port_pos = temp.find(b":")
+    webserver_pos = temp.find(b"/")
     if webserver_pos == -1:
         webserver_pos = len(temp)
 
     webserver = ""
     port = -1
-    if (port_pos==-1 or webserver_pos < port_pos):      # default port
+    if port_pos == -1 or webserver_pos < port_pos:
         port = 80
         webserver = temp[:webserver_pos]
-    else:       # specific port
-        port = int((temp[(port_pos+1):])[:webserver_pos-port_pos-1])
+    else:
+        port = int(temp[port_pos + 1:webserver_pos].decode())
         webserver = temp[:port_pos]
 
     try:
-        # create a socket to connect to the web server
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  
-        s.connect((webserver, port))
-        s.send(request)         # send request to webserver
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect((webserver.decode(), port))
+        s.send(request)
 
-        while 1:
-            # receive data from web server
+        while True:
             data = s.recv(MAX_DATA_RECV)
-
-            if (len(data) > 0):
-                # send to browser
+            if len(data) > 0:
                 conn.send(data)
             else:
                 break
         s.close()
         conn.close()
-    except socket.error, (value, message):
+    except socket.error as e:
         if s:
             s.close()
         if conn:
             conn.close()
-        printout("Peer Reset",first_line,client_addr)
+        printout("Peer Reset", first_line, client_addr)
+
+def main():
+    if len(sys.argv) < 2:
+        port = 8080
+    else:
+        port = int(sys.argv[1])
+
+    host = ""
+    print(f"Proxy Server Running on {host}:{port}")
+
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.bind((host, port))
+        s.listen(BACKLOG)
+
+        while True:
+            conn, client_addr = s.accept()
+            threading.Thread(target=proxy_thread, args=(conn, client_addr)).start()
+
+    except socket.error as e:
+        if s:
+            s.close()
+        print(f"Could not open socket: {e}")
         sys.exit(1)
-#********** END PROXY_THREAD ***********
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
-
